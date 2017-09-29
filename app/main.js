02754a0739d1b53a4d322cccdc759d036cfc6ce6
trenshaw88 <trenshaw88@twc.com>
2017-09-29 17:00:44 -0400

    Added main.js

---
+++ app/main.js
@@ -0,0 +1,15 @@
+// var 
+// Globals
+// var regFlav = ;
+// var seasFlav =;
+// var servOp = ;
+// var topping = ;
+
+
+// var = document.getElementByID('');
+
+
+
+// var type = document.getElementsByClassName('type');
+
+// var itemList = document.getElementsByClassName('item-list');
